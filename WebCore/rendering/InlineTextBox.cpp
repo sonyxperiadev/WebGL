3620888aeeb06e4dd2960323413709461cab663b
Leon Scroggins <>
2009-04-03 09:43:49 -0700

    AI 144458: Remove our hack to prevent drawing the selection, and set our selection color to transparent.  This gets us the same result while simplifying the merge.
    Automated import of CL 144458

--- WebCore/rendering/InlineTextBox.cpp
+++ WebCore/rendering/InlineTextBox.cpp
@@ -498,17 +498,6 @@ void InlineTextBox::paintSelection(GraphicsContext* context, int tx, int ty, Ren
     selectionStartEnd(sPos, ePos);
     if (sPos >= ePos)
         return;
-#ifdef ANDROID_DO_NOT_DRAW_TEXTFIELD_SELECTION
-    Node* element = m_object->element();
-    if (element) {
-        Node* ancestor = element->shadowAncestorNode();
-        if (ancestor && ancestor->renderer()) {
-            RenderObject* renderer = ancestor->renderer();
-            if (renderer->isTextField() || renderer->isTextArea())
-                return;
-        }
-    }
-#endif
 
     Color textColor = style->color();
     Color c = object()->selectionBackgroundColor();
