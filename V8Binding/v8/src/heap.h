3e119f4f9bb36bc17675f4b60284508e1400944c
Feng Qian <fqian@google.com>
2009-06-23 09:44:26 -0700

    Workaround gcc4.4. issue.
    I am upstreaming the change to v8 code.

--- V8Binding/v8/src/heap.h
+++ V8Binding/v8/src/heap.h
@@ -936,6 +936,14 @@ class Heap : public AllStatic {
 
   static bool CreateInitialMaps();
   static bool CreateInitialObjects();
+
+  // These four Create*EntryStub functions are here because of a gcc-4.4 bug
+  // that assign wrong vptr entries.
+  static void CreateCEntryStub();
+  static void CreateCEntryDebugBreakStub();
+  static void CreateJSEntryStub();
+  static void CreateJSConstructEntryStub();
+
   static void CreateFixedStubs();
   static Object* CreateOddball(Map* map,
                                const char* to_string,
