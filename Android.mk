df4415ecdcdca0324b9535717089347b3ecb7fee
Feng Qian <fqian@google.com>
2009-07-15 09:40:17 -0700

    Use JS_ENGINE environment to control which JS engine to use.

--- Android.mk
+++ Android.mk
@@ -17,8 +17,27 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(USE_ALT_JS_ENGINE),true)
-include $(LOCAL_PATH)/Android.v8.mk
+# Two ways to control which JS engine is used:
+# 1. use JS_ENGINE environment variable, value can be either 'jsc' or 'v8'
+#    This is the preferred way.
+# 2. if JS_ENGINE is not set, or is not 'jsc' or 'v8', this makefile picks
+#    up a default engine to build.
+#    To help setup buildbot, a new environment variable, USE_ALT_JS_ENGINE,
+#    can be set to true, so that two builds can be different but without
+#    specifying which JS engine to use.
+
+# Check JS_ENGINE environment variable
+ifeq ($(JS_ENGINE),jsc)
+  include $(LOCAL_PATH)/Android.jsc.mk
 else
-include $(LOCAL_PATH)/Android.jsc.mk
-endif
+  ifeq ($(JS_ENGINE),v8)
+    include $(LOCAL_PATH)/Android.v8.mk
+  else
+    # No JS engine is specified, pickup the one we want as default.
+    ifeq ($(USE_ALT_JS_ENGINE),true)
+      include $(LOCAL_PATH)/Android.v8.mk
+    else
+      include $(LOCAL_PATH)/Android.jsc.mk
+    endif  // USE_ALT_JS_ENGINE == true
+  endif  // JS_ENGINE == v8
+endif // JS_ENGINE == jsc
