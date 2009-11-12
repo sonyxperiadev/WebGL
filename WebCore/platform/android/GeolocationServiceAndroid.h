e03118fd5d9b776df29de3f96232ed6b5395c845
Steve Block <steveblock@google.com>
2009-11-12 23:26:11 +0000

    Fixes license headers for all Android-specific JavaScriptCore and WebCore files not yet upstreamed to webkit.org.
    WebKit requires either a BSD-style or LGPL 2.1 license for all code.
    We use a BSD-style 2-clause license for Android-specific files that will be upstreamed to webkit.org.
    
    - For most files, I've fixed the names of copyright holders in the license text to 'THE COPYRIGHT HOLDERS'
      and cleaned up formatting in the existing BSD-style license.
    - For Makefiles and the following files in WebCore/platform/graphics/android/, I've changed from an
      Apache 2.0 license to the BSD-style license.
      - WebCore/platform/graphics/android/BitmapAllocatorAndroid.cpp
      - WebCore/platform/graphics/android/BitmapAllocatorAndroid.h
      - WebCore/platform/graphics/android/SharedBufferStream.cpp
      - WebCore/platform/graphics/android/SharedBufferStream.h
    - For the following files, I've changed from an LGPL licese to the BSD-style license.
      - WebCore/platform/graphics/android/FontPlatformData.h
      - WebCore/platform/graphics/android/FontPlatformDataAndroid.cpp
      - WebCore/plugins/android/PluginDataAndroid.cpp
    
    Change-Id: Ic4c3e5610afc58637b7a9b81b1d0d1f17f4f4cb1

--- WebCore/platform/android/GeolocationServiceAndroid.h
+++ WebCore/platform/android/GeolocationServiceAndroid.h
@@ -1,19 +1,19 @@
 /*
- * Copyright (C) 2009 Apple Inc. All Rights Reserved.
+ * Copyright 2009, The Android Open Source Project
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 1. Redistributions of source code must retain the above copyright
+ *  * Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
+ *  * Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
  * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
