8dcb4bb39afc3d3e0f58ecee412a420fe0290076
Steve Block <steveblock@google.com>
2009-11-17 13:39:58 +0000

    Cleans up Android-specific changes to WebCore/bridge/jni ready for upstreaing to webkit.org.
    Change-Id: I0778955bee95680a9924fdae2967158e4add81ba

--- WebCore/bridge/jni/jni_utility.cpp
+++ WebCore/bridge/jni/jni_utility.cpp
@@ -28,12 +28,14 @@
 
 #if ENABLE(MAC_JAVA_BRIDGE)
 
+#if PLATFORM(ANDROID)
 #if USE(JSC)
 #include "jni_runtime.h"
 #include "runtime_array.h"
 #include "runtime_object.h"
 #include <runtime/JSArray.h>
 #include <runtime/JSLock.h>
+#endif
 #endif
 
 #include <dlfcn.h>
@@ -350,6 +352,7 @@ jvalue getJNIField( jobject obj, JNIType type, const char *name, const char *sig
     return result;
 }
 
+#if PLATFORM(ANDROID)
 #if USE(JSC)
 static jobject convertArrayInstanceToJavaArray(ExecState* exec, JSArray* jsArray, const char* javaClassName)
 {
@@ -581,6 +584,7 @@ jvalue convertValueToJValue(ExecState* exec, JSValue value, JNIType _JNIType, co
     return result;
 }
 #endif  // USE(JSC)
+#endif  // PLATFORM(ANDROID)
 
 }  // end of namespace Bindings
 
