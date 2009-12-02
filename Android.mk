edac9050e2c3239d294f9039c54a31058ab0a783
Jean-Baptiste Queru <jbq@google.com>
2009-12-01 19:13:09 -0800

    Revert "Don't use the WebKit STL when building the simulator"
    This reverts commit afa94bcc85b11e170818a896b14f38521d11d333.

--- Android.mk
+++ Android.mk
@@ -211,12 +211,8 @@ LOCAL_C_INCLUDES := $(LOCAL_C_INCLUDES) \
 	$(LOCAL_PATH)/WebKit/android/WebCoreSupport \
 	$(LOCAL_PATH)/WebKit/android/jni \
 	$(LOCAL_PATH)/WebKit/android/nav \
-	$(LOCAL_PATH)/WebKit/android/plugins
-
-ifneq ($(TARGET_SIMULATOR),true)
-LOCAL_C_INCLUDES := $(LOCAL_C_INCLUDES) \
+	$(LOCAL_PATH)/WebKit/android/plugins \
 	$(LOCAL_PATH)/WebKit/android/stl
-endif
 
 LOCAL_C_INCLUDES := $(LOCAL_C_INCLUDES) \
 	$(LOCAL_PATH)/JavaScriptCore \
