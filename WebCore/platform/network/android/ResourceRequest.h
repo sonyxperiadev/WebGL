730ef1944495761bb07f38e435da91f1ad059972
Andrei Popescu <andreip@google.com>
2009-12-14 12:45:34 +0000

    Replace the C-cast used to determine whether the resource handle represent the main resource.
    Fix style to match webkit style.

--- WebCore/platform/network/android/ResourceRequest.h
+++ WebCore/platform/network/android/ResourceRequest.h
@@ -33,41 +33,30 @@
 
 namespace WebCore {
 
-    struct ResourceRequest : ResourceRequestBase {
+class ResourceRequest : public ResourceRequestBase {
+public:
+    ResourceRequest(const String& url)
+        : ResourceRequestBase(KURL(ParsedURLString, url), UseProtocolCachePolicy), m_userGesture(true) { }
 
-        ResourceRequest(const String& url) 
-            : ResourceRequestBase(KURL(ParsedURLString, url), UseProtocolCachePolicy)
-            , m_userGesture(true)
-        {
-        }
+    ResourceRequest(const KURL& url) : ResourceRequestBase(url, UseProtocolCachePolicy) , m_userGesture(true) { }
 
-        ResourceRequest(const KURL& url) 
-            : ResourceRequestBase(url, UseProtocolCachePolicy)
-            , m_userGesture(true)
-        {
-        }
+    ResourceRequest(const KURL& url, const String& referrer, ResourceRequestCachePolicy policy = UseProtocolCachePolicy)
+        : ResourceRequestBase(url, policy) , m_userGesture(true)
+    {
+        setHTTPReferrer(referrer);
+    }
 
-        ResourceRequest(const KURL& url, const String& referrer, ResourceRequestCachePolicy policy = UseProtocolCachePolicy) 
-            : ResourceRequestBase(url, policy)
-            , m_userGesture(true)
-        {
-            setHTTPReferrer(referrer);
-        }
-        
-        ResourceRequest()
-            : ResourceRequestBase(KURL(), UseProtocolCachePolicy)
-            , m_userGesture(true)
-        {
-        }
-        
-        void doUpdatePlatformRequest() {}
-        void doUpdateResourceRequest() {}
-        void setUserGesture(bool userGesture) { m_userGesture = userGesture; }
-        bool getUserGesture() const { return m_userGesture; }
-    private:
-        friend class ResourceRequestBase;
-        bool m_userGesture;
-    };
+    ResourceRequest() : ResourceRequestBase(KURL(), UseProtocolCachePolicy), m_userGesture(true) { }
+
+    void doUpdatePlatformRequest() { }
+    void doUpdateResourceRequest() { }
+    void setUserGesture(bool userGesture) { m_userGesture = userGesture; }
+    bool getUserGesture() const { return m_userGesture; }
+
+private:
+    friend class ResourceRequestBase;
+    bool m_userGesture;
+};
 
 } // namespace WebCore
 
