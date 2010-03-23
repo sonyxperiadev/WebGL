7d7e576868fe78bf1b95968ad5d6ddc395c65ec9
Daisuke Miyakawa <dmiyakawa@google.com>
2010-03-23 11:07:30 +0900

    Use EmojiFoctory::GetShiftJisConverterName() instead of relying on static string.
    This change should be with the change Iccbef288.
    
    Bug: 2327318
    Change-Id: Ie67d6c1ef60d8e57784e903e3e00a73d3bec0f82

--- WebKit/android/icu/unicode/ucnv.cpp
+++ WebKit/android/icu/unicode/ucnv.cpp
@@ -40,8 +40,9 @@ namespace android {
 U_STABLE UConverter* U_EXPORT2
 ucnv_open_emoji(const char *converterName, UErrorCode *err) {
     if (EmojiFont::IsAvailable()) {
-        if (strcmp(converterName, "Shift_JIS") == 0)
-            converterName = "docomo-emoji";
+        if (strcmp(converterName, "Shift_JIS") == 0) {
+            converterName = EmojiFont::GetShiftJisConverterName();
+        }
     }
     return ucnv_open(converterName, err);
 }
