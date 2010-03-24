6b3a78a9b31b2bf20a2a75fef79d9c943a461e74
Dan Egnor <egnor@google.com>
2010-03-24 15:32:36 -0700

    Apparently this needs to happen again (at least, it did for my build).
    Change-Id: I89dbec1be74da68afb2a03b75ad5bf87426d8889

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -46,6 +46,8 @@
 
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/STATIC_LIBRARIES/libwebcore_intermediates)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES/libwebcore_intermediates)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/STATIC_LIBRARIES/libwebcore_intermediates)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES/libwebcore_intermediates)
 
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
