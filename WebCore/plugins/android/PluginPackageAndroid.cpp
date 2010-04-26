af051345af6893a284d0ce367aa92a6d8a977f38
Steve Block <steveblock@google.com>
2010-04-25 18:35:48 -0700

    Merge webkit.org at r55033 : Implement PluginPackage::NPVersion for Android
    See http://trac.webkit.org/changeset/54940
    
    Change-Id: I2c3153606ab0eb9a4598e211def2f07f60eab3da

--- WebCore/plugins/android/PluginPackageAndroid.cpp
+++ WebCore/plugins/android/PluginPackageAndroid.cpp
@@ -407,6 +407,11 @@ bool PluginPackage::equal(const PluginPackage& a, const PluginPackage& b)
     return true;
 }
 
+uint16 PluginPackage::NPVersion() const
+{
+    return NP_VERSION_MINOR;
+}
+
 } // namespace WebCore
 
 #endif
