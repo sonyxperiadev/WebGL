2328ced4730eaceb3fad20e9118763dc3c4c253d
Steve Block <steveblock@google.com>
2010-04-29 14:58:23 +0100

    Merge webkit.org at r58033 : Update stubbed methods in InspectorClientAndroid
    See http://trac.webkit.org/changeset/56051
    
    Change-Id: I6b30d9fbb157ec77e8ba93cb0fa9a6e0c2a9ec81

--- WebKit/android/WebCoreSupport/InspectorClientAndroid.h
+++ WebKit/android/WebCoreSupport/InspectorClientAndroid.h
@@ -36,27 +36,13 @@ public:
 
     virtual void inspectorDestroyed() { delete this; }
 
-    virtual Page* createPage() { return NULL; }
-
-    virtual String localizedStringsURL() { return String(); }
-
-    virtual void showWindow() {}
-    virtual void closeWindow() {}
-
-    virtual void attachWindow() {}
-    virtual void detachWindow() {}
-
-    virtual void setAttachedWindowHeight(unsigned height) {}
+    virtual void openInspectorFrontend(WebCore::InspectorController*) {}
 
     virtual void highlight(Node*) {}
     virtual void hideHighlight() {}
 
-    virtual void inspectedURLChanged(const String& newURL) {}
-
     virtual void populateSetting(const String& key, String* value) {}
     virtual void storeSetting(const String& key, const String& value) {}
-    virtual String hiddenPanels() { return String(); }
-    virtual void inspectorWindowObjectCleared() {}
 };
 
 }
