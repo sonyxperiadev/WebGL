cc87a1a6e60b579d555cc1f1d2b44b52da6becda
Leon Clarke <leonclarke@google.com>
2010-06-03 16:15:49 +0100

    Merge webkit.org at r60469 : toDataURL takes a quality hint since webkit @60458 http://trac.webkit.org/changeset/60458
    Change-Id: I8a7bc06b4a5b4157161a7bef3bbbebd600d511d4

--- WebCore/platform/graphics/android/ImageBufferAndroid.cpp
+++ WebCore/platform/graphics/android/ImageBufferAndroid.cpp
@@ -209,7 +209,7 @@ void ImageBuffer::putUnmultipliedImageData(ImageData* source, const IntRect& sou
 }
 
     
-String ImageBuffer::toDataURL(const String&) const
+String ImageBuffer::toDataURL(const String&, double) const
 {  
     // Encode the image into a vector.
     SkDynamicMemoryWStream pngStream;
