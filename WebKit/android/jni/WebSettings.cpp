6f60177b50520b5406116adc50e752cb8a596243
Leon Clarke <leonclarke@google.com>
2010-06-03 17:53:38 +0100

    Merge webkit.org at r60469 : since @60104 the zoom level functions ar in the FrameView http://trac.webkit.org/changeset/60104
    Change-Id: I7050bd945d397552bd154490277881cb4cc961c9

--- WebKit/android/jni/WebSettings.cpp
+++ WebKit/android/jni/WebSettings.cpp
@@ -261,8 +261,8 @@ public:
 #endif
         jobject textSize = env->GetObjectField(obj, gFieldIds->mTextSize);
         float zoomFactor = env->GetIntField(textSize, gFieldIds->mTextSizeValue) / 100.0f;
-        if (pFrame->zoomFactor() != zoomFactor)
-            pFrame->setZoomFactor(zoomFactor, WebCore::ZoomTextOnly);
+        if (pFrame->view()->zoomFactor() != zoomFactor)
+            pFrame->view()->setZoomFactor(zoomFactor, WebCore::ZoomTextOnly);
 
         jstring str = (jstring)env->GetObjectField(obj, gFieldIds->mStandardFontFamily);
         s->setStandardFontFamily(to_string(env, str));
