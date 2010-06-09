27009bcd4f0d3c6fb5f1324521a353c8fdfd168c
Steve Block <steveblock@google.com>
2010-06-09 11:21:20 +0100

    Cherry-pick WebKit change r60069 to fix a leak in Geolocation
    This change was made as a fix for Bug https://bugs.webkit.org/show_bug.cgi?id=39288
    but was later rolled back in r60441 pending further discussion.
    
    We need this fix, so cherry-pick it to Android until
    https://bugs.webkit.org/show_bug.cgi?id=39288 is fixed properly.
    
    Change-Id: Ie5cc610b841798e42ec1232b4af7674ce425b2c4

--- WebCore/page/Geolocation.h
+++ WebCore/page/Geolocation.h
@@ -65,6 +65,10 @@ public:
     void suspend();
     void resume();
 
+#if PLATFORM(ANDROID)
+    void stop();
+#endif // PLATFORM(ANDROID)
+
     void setIsAllowed(bool);
     Frame* frame() const { return m_frame; }
 
