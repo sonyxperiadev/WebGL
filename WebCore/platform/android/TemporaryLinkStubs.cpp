8442e31d83ef390f46627b7fe09ed76a5e64e6d7
Ben Murdoch <benm@google.com>
2010-07-22 17:05:30 +0100

    Merge Webkit at r63859 : The static AccessibilityObject::getAttribute(Node*, const QualfiedName&) function has been removed. See http://trac.webkit.org/changeset/63358
    Change-Id: I89c6a0a98d04f0a0cd40492229fa03f2e9782622

--- WebCore/platform/android/TemporaryLinkStubs.cpp
+++ WebCore/platform/android/TemporaryLinkStubs.cpp
@@ -473,12 +473,6 @@ void AXObjectCache::remove(RenderObject*)
     notImplemented();
 }
 
-const AtomicString& AccessibilityObject::getAttribute(Node*, const QualifiedName&)
-{
-    static const AtomicString emptyString;
-    return emptyString;
-}
-
 #if USE(JSC)
 using namespace JSC;
 
