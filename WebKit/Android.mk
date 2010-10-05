d9b2f2e1e22ec3dfc73795d9e5476c7ab7ddb43d
Steve Block <steveblock@google.com>
2010-10-05 14:28:30 +0100

    Implement DeviceMotionEvent
    Requires a corresponding change in frameworks/base ...
    https://android-git.corp.google.com/g/72907
    
    Bug: 2495651
    Change-Id: I8ba07a0280b96379b3717a707184d719da6266bf

--- WebKit/Android.mk
+++ WebKit/Android.mk
@@ -57,6 +57,7 @@ LOCAL_SRC_FILES := $(LOCAL_SRC_FILES) \
 	\
 	android/icu/unicode/ucnv.cpp \
 	\
+	android/jni/DeviceMotionClientImpl.cpp \
 	android/jni/DeviceOrientationClientImpl.cpp \
 	android/jni/DeviceOrientationManager.cpp \
 	android/jni/GeolocationPermissionsBridge.cpp \
---
+++ WebKit/android/jni/DeviceMotionClientImpl.cpp
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2010, The Android Open Source Project
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "DeviceMotionClientImpl.h"
+
+#include "WebViewCore.h"
+#include <DeviceMotionController.h>
+#include <Frame.h>
+#include <JNIHelp.h>
+
+namespace android {
+
+using JSC::Bindings::getJNIEnv;
+
+enum javaServiceClassMethods {
+    ServiceMethodStart = 0,
+    ServiceMethodStop,
+    ServiceMethodSuspend,
+    ServiceMethodResume,
+    ServiceMethodCount
+};
+static jmethodID javaServiceClassMethodIDs[ServiceMethodCount];
+
+DeviceMotionClientImpl::DeviceMotionClientImpl(WebViewCore* webViewCore)
+    : m_webViewCore(webViewCore)
+    , m_javaServiceObject(0)
+{
+    ASSERT(m_webViewCore);
+}
+
+DeviceMotionClientImpl::~DeviceMotionClientImpl()
+{
+    releaseJavaInstance();
+}
+
+jobject DeviceMotionClientImpl::getJavaInstance()
+{
+    // Lazily get the Java object. We can't do this until the WebViewCore is all
+    // set up.
+    if (m_javaServiceObject)
+        return m_javaServiceObject;
+
+    JNIEnv* env = getJNIEnv();
+
+    ASSERT(m_webViewCore);
+    jobject object = m_webViewCore->getDeviceMotionService();
+
+    // Get the Java DeviceMotionService class.
+    jclass javaServiceClass = env->GetObjectClass(object);
+    ASSERT(javaServiceClass);
+
+    // Set up the methods we wish to call on the Java DeviceMotionService
+    // class.
+    javaServiceClassMethodIDs[ServiceMethodStart] =
+        env->GetMethodID(javaServiceClass, "start", "()V");
+    javaServiceClassMethodIDs[ServiceMethodStop] =
+        env->GetMethodID(javaServiceClass, "stop", "()V");
+    javaServiceClassMethodIDs[ServiceMethodSuspend] =
+        env->GetMethodID(javaServiceClass, "suspend", "()V");
+    javaServiceClassMethodIDs[ServiceMethodResume] =
+        env->GetMethodID(javaServiceClass, "resume", "()V");
+
+    m_javaServiceObject = getJNIEnv()->NewGlobalRef(object);
+    getJNIEnv()->DeleteLocalRef(object);
+
+    ASSERT(m_javaServiceObject);
+    return m_javaServiceObject;
+}
+
+void DeviceMotionClientImpl::releaseJavaInstance()
+{
+    ASSERT(m_javaServiceObject);
+    getJNIEnv()->DeleteGlobalRef(m_javaServiceObject);
+}
+
+void DeviceMotionClientImpl::startUpdating()
+{
+    getJNIEnv()->CallVoidMethod(getJavaInstance(),
+        javaServiceClassMethodIDs[ServiceMethodStart]);
+}
+
+void DeviceMotionClientImpl::stopUpdating()
+{
+    getJNIEnv()->CallVoidMethod(getJavaInstance(),
+        javaServiceClassMethodIDs[ServiceMethodStop]);
+}
+
+void DeviceMotionClientImpl::onMotionChange(PassRefPtr<DeviceMotionData> motion)
+{
+    m_lastMotion = motion;
+    m_controller->didChangeDeviceMotion(m_lastMotion.get());
+}
+
+void DeviceMotionClientImpl::suspend()
+{
+    getJNIEnv()->CallVoidMethod(getJavaInstance(),
+        javaServiceClassMethodIDs[ServiceMethodSuspend]);
+}
+
+void DeviceMotionClientImpl::resume()
+{
+    getJNIEnv()->CallVoidMethod(getJavaInstance(),
+        javaServiceClassMethodIDs[ServiceMethodResume]);
+}
+
+} // namespace android
