390c2b5702d131f0875199fdca1c3ba5010d7659
Leon Scroggins <scroggo@google.com>
2010-10-12 14:01:13 -0400

    Provide padding + border values for textfields.
    Bug:3085564
    
    They are used by WebView.java to align the WebTextView's
    text positioning with the page below.  This allows the
    selection and insertion arrow handlers to line up better
    with the actual selection on the page.
    
    Requires a change in frameworks/base:
    https://android-git.corp.google.com/g/#change,73565
    
    Change-Id: Ia100286016a780abd5f30e32975f8ad566a6ed74

--- WebKit/android/nav/CachedInput.h
+++ WebKit/android/nav/CachedInput.h
@@ -48,20 +48,32 @@ public:
     bool isTextField() const { return mIsTextField; }
     int maxLength() const { return mMaxLength; };
     const WebCore::String& name() const { return mName; }
+    int paddingBottom() const { return mPaddingBottom; }
+    int paddingLeft() const { return mPaddingLeft; }
+    int paddingRight() const { return mPaddingRight; }
+    int paddingTop() const { return mPaddingTop; }
     void setFormPointer(void* form) { mForm = form; }
     void setInputType(WebCore::HTMLInputElement::InputType type) { mInputType = type; }
     void setIsRtlText(bool isRtlText) { mIsRtlText = isRtlText; }
     void setIsTextField(bool isTextField) { mIsTextField = isTextField; }
     void setMaxLength(int maxLength) { mMaxLength = maxLength; }
     void setName(const WebCore::String& name) { mName = name; }
+    void setPaddingBottom(int bottom) { mPaddingBottom = bottom; }
+    void setPaddingLeft(int left) { mPaddingLeft = left; }
+    void setPaddingRight(int right) { mPaddingRight = right; }
+    void setPaddingTop(int top) { mPaddingTop = top; }
     void setTextSize(int textSize) { mTextSize = textSize; }
     int textSize() const { return mTextSize; }
 private:
     void* mForm;
-    WebCore::String mName;
-    int mMaxLength;
-    int mTextSize;
     WebCore::HTMLInputElement::InputType mInputType;
+    int mMaxLength;
+    WebCore::String mName;
+    int mPaddingBottom;
+    int mPaddingLeft;
+    int mPaddingRight;
+    int mPaddingTop;
+    int mTextSize;
     bool mIsRtlText : 1;
     bool mIsTextField : 1;
 #if DUMP_NAV_CACHE
