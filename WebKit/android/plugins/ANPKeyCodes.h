e5240a0d361862d4ab0a9b89728d09d638be3dee
Jeff Brown <jeffbrown@google.com>
2010-11-01 15:24:52 -0700

    Add volume mute key.
    Also fixed incorrect keycode constant for numpad parens.
    
    Bug: 2912307
    Change-Id: I65d02ca1c131638ea70f333bf06982747f34223c

--- WebKit/android/plugins/ANPKeyCodes.h
+++ WebKit/android/plugins/ANPKeyCodes.h
@@ -196,7 +196,8 @@ enum ANPKeyCodes {
     kNumPadEnter_ANPKeyCode = 160,
     kNumPadEquals_ANPKeyCode = 161,
     kNumPadLeftParen_ANPKeyCode = 162,
-    kNumPadRightParen_ANPKeyCode = 162,
+    kNumPadRightParen_ANPKeyCode = 163,
+    kVolumeMute_ANPKeyCode = 164,
 
     // NOTE: If you add a new keycode here you must also add it to several other files.
     //       Refer to frameworks/base/core/java/android/view/KeyEvent.java for the full list.
