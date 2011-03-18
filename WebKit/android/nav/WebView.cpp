be25f896a5c754e96c6c0d4719a79d523293d40f
Nicolas Roard <nicolasroard@google.com>
2011-03-18 15:42:12 -0700

    Fix for missing invals
    bug:4124433
    Change-Id: I8cc7203dad408eff30da33f1c9a0a77dd7c97d66

--- WebKit/android/nav/WebView.cpp
+++ WebKit/android/nav/WebView.cpp
@@ -1509,7 +1509,7 @@ class GLDrawFunctor : Functor {
             IntRect finalInval;
             if (inval.isEmpty()) {
                 finalInval = webViewRect;
-                retVal = false;
+                retVal = true;
             } else {
                 finalInval.setX(webViewRect.x() + inval.x());
                 finalInval.setY(webViewRect.y() + inval.y() + titlebarHeight);
