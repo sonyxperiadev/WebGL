03137dd16df459ef3bee925373347982cb98124d
Teng-Hui Zhu <ztenghui@google.com>
2011-08-03 10:02:57 -0700

    increase layer tiles limit
    bug:5069164
    
    Change-Id: Ifbba6d912e20868d0618441b7f497270e1d9f737

--- Source/WebCore/platform/graphics/android/TilesManager.cpp
+++ Source/WebCore/platform/graphics/android/TilesManager.cpp
@@ -75,7 +75,7 @@
 #define TILE_HEIGHT 256
 #define LAYER_TILE_WIDTH 256
 #define LAYER_TILE_HEIGHT 256
-#define LAYER_TILES 10
+#define LAYER_TILES 50
 
 // Define a maximum amount of ram used by layers
 #define MAX_LAYERS_ALLOCATION 33554432 // 32Mb
